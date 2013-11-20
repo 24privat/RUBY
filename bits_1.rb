15f6391e15cd5b6b90e427bba6afe02634f6a354
Oleg <24privat@users.noreply.github.com>
2013-11-20 17:32:31 +0200

    Update bits_1.rb

--- bits_1.rb
+++ bits_1.rb
@@ -474,3 +474,18 @@ end
 
 ------LEVEL 6------
 
+1.
+class Library
+  attr_accessor :games
+
+  def initialize(games = [])
+    self.games = games
+  end
+
+  def list
+    games.each do |game|
+      puts game.name
+    end
+  end
+end
+
