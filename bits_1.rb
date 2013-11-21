843832397a767f01e906f6f8bc1733fc193e1b98
Oleg <24privat@users.noreply.github.com>
2013-11-21 17:11:20 +0200

    Update bits_1.rb

--- bits_1.rb
+++ bits_1.rb
@@ -489,3 +489,17 @@ class Library
   end
 end
 
+2.
+class Library
+  attr_accessor :games
+
+  def initialize(games = [])
+    self.games = games
+  end
+
+  def each_on_system(system)
+    games.each do |game|
+      yield if game.system == system
+    end
+  end
+end
