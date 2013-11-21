3d0152f62f8ef80824d475c77e25511bfe5527f3
Oleg <24privat@users.noreply.github.com>
2013-11-21 17:17:24 +0200

    Update bits_1.rb

--- bits_1.rb
+++ bits_1.rb
@@ -550,3 +550,40 @@ class Library
     end
   end
 end
+
+6.
+class Game
+  attr_accessor :name, :year, :system
+  attr_reader :created_at
+
+  def initialize(name, options={})
+    self.name = name
+    self.year = options[:year]
+    self.system = options[:system]
+    @created_at = Time.now
+  end
+
+  def play
+    emulate do |emulator|
+      emulator.play(self)
+    end
+  end
+
+  def screenshot
+    emulate do |emulator|
+      emulator.start(self)
+      emulator.screenshot
+    end
+  end
+
+  private
+
+  def emulate
+    begin
+      emulator = Emulator.new(system)
+      yield emulator
+    rescue Exception => e
+      puts "Emulator failed: #{e}"
+    end
+  end
+end
