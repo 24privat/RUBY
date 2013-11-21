3adf9849df22f77df68485f08ac5f87a1a26252e
Oleg <24privat@users.noreply.github.com>
2013-11-21 17:13:56 +0200

    Update bits_1.rb

--- bits_1.rb
+++ bits_1.rb
@@ -518,3 +518,18 @@ class Library
     end
   end
 end
+
+4.
+class Library
+  attr_accessor :games
+
+  def initialize(games = [])
+    self.games = games
+  end
+
+  def list
+    games.each do |game|
+    puts  yield game
+    end
+  end
+end
