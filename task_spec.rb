e0263e78238c29e7e1d798a6237c5f06699d3b45
Oleg <24privat@users.noreply.github.com>
2013-11-27 15:41:27 +0200

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -84,3 +84,18 @@ describe Plant do
   subject { plant }
   it_behaves_like 'the brainless'
 end
+
+5. describe Zombie do
+  let(:zombie) { Zombie.new }
+  subject { zombie }
+
+  context 'with a dummy zombie' do
+    before { zombie.iq = 0 }
+    it { should be_dummy }
+  end
+
+  context 'with a smart zombie' , focus: true do
+    before { zombie.iq = 3 }
+    it { should_not be_dummy }
+  end
+end
