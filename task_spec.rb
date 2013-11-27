215bbcb6258ba0607ddfd1d3675e6d275fab8a10
Oleg <24privat@users.noreply.github.com>
2013-11-27 14:59:51 +0200

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -1,3 +1,14 @@
--- LEVEL 2 --
+-- LEVEL 3 --
 
-1.
+3. describe Zombie do
+  it { should_not be_genius }
+  its(:iq) { should == 0 }
+
+  context "with high iq" do
+    subject { Zombie.new(iq: 3) }
+    it { should be_genius }
+    its(:brains_eaten_count) { should == 1 }
+  end
+end
+
+4.
