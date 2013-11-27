eb333d30a934507196fd6b336f1b9eb19ad74f73
Oleg <24privat@users.noreply.github.com>
2013-11-27 15:29:09 +0200

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -39,3 +39,16 @@ end
     zombie.should be_genius
   end
 end
+
+2. describe Zombie do
+  let(:zombie) { Zombie.new }
+  before { zombie.iq = 0 }
+  subject { zombie }
+
+  it { should be_dummy }
+
+  context 'with a smart zombie' do
+    before { zombie.iq = 3 }
+    it { should_not be_dummy }
+  end
+end
