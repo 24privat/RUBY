c6d35a2cf389d188c3217607382e12fcee762d23
Oleg <24privat@users.noreply.github.com>
2013-11-28 15:38:39 +0200

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -115,4 +115,16 @@ end
   end
 end
 
+2. describe ZombieMailer do
+  context '#tweet' do
+    let(:zombie) { stub(:zombie, email: 'anything@example.org') }
+    let(:tweet) { stub(:tweet, message: 'Arrrrgggghhhh') }
+
+    subject { ZombieMailer.tweet(zombie, tweet) }
+    its(:from) { should include('admin@codeschool.com') }
+    its(:to) { should include(zombie.email) }
+    its(:subject) { should == tweet.message }
+  end
+end
+
 
