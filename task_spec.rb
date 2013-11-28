57323bd8403a47722101b58a25d2d188a5bc4700
Oleg <24privat@users.noreply.github.com>
2013-11-28 15:42:37 +0200

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -127,4 +127,16 @@ end
   end
 end
 
+3. describe Tweet do
+  context 'after create' do
+    let(:zombie) { Zombie.create(email: 'anything@example.org') }
+    let(:tweet) { zombie.tweets.new(message: 'Arrrrgggghhhh') }
+
+    it 'calls "email_tweeter" on the tweet' do
+      tweet.should_receive(:email_tweeter)
+      tweet.save
+    end
+  end
+end
+
 
