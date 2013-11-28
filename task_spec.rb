f2bf7fdbadcccf14b8d0552ef27db908ef6f97b2
Oleg <24privat@users.noreply.github.com>
2013-11-28 16:14:09 +0200

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -203,3 +203,34 @@ end
 end
 
 -- LEVEL 6 --
+
+1. module ValidateNumericalityOf
+  class Matcher
+    def initialize(attribute)
+    end
+
+    def matches?(model)
+      false
+    end
+
+    def failure_message
+      "Not implemented"
+    end
+
+    def negative_failure_message
+      "Not implemented"
+    end
+
+    def description
+      "Not implemented"
+    end
+  end
+
+  def validate_numericality_of(attribute)
+    pending
+  end
+end
+
+RSpec.configure do |config|
+  config.include ValidateNumericalityOf, type: :model
+end
