af621f75a28482fd9ffcb21135e1290a44522136
Oleg <24privat@users.noreply.github.com>
2013-11-28 15:32:20 +0200

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -99,3 +99,20 @@ end
     it { should_not be_dummy }
   end
 end
+
+
+-- LEVEL 5 --
+
+1. describe ZombieMailer do
+  context '#tweet' do
+    let(:zombie) { stub(email: 'anything@example.org') }
+    let(:tweet) { stub(message: 'Arrrrgggghhhh') }
+
+    subject { ZombieMailer.tweet(zombie, tweet) }
+    its(:from) { should include('admin@codeschool.com') }
+    its(:to) { should include(zombie.email) }
+    its(:subject) { should == tweet.message }
+  end
+end
+
+
