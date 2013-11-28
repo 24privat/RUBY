e95d60218f662a16955c24cb3a145bffbdc53e3d
Oleg <24privat@users.noreply.github.com>
2013-11-28 16:24:14 +0200

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -305,3 +305,42 @@ RSpec.configure do |config|
   config.include ValidateNumericalityOf, type: :model
 end
 
+4. module ValidateNumericalityOf
+  class Matcher
+    def initialize(attribute)
+      @attribute = attribute
+    end
+
+    def matches?(model)
+      @model = model
+      check_for_number
+    end
+
+    def failure_message
+      "#{@model.class} failed to validate: #{@attribute} numericality"
+    end
+
+    def negative_failure_message
+      "#{@model.class} unexpected validation: #{@attribute} numericality"
+    end
+
+    def description
+      "validate numericality of #{@attribute}"
+    end
+    
+    def check_for_number
+      @model[@attribute] = "not a number"
+      @model.valid?
+      @model.errors[@attribute].include?("is not a number")
+    end
+  end
+
+  def validate_numericality_of(attribute)
+    Matcher.new(attribute)
+  end
+end
+
+RSpec.configure do |config|
+  config.include ValidateNumericalityOf, type: :model
+end
+
